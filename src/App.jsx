b722509596713bea1f980c3265552b4be466508c
copperlamb78 <nascimentogabriel.2004@gmail.com>
2024-05-05 20:13:39 -0300

    resolve problem commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 
 import { Introduction } from './components/introdução'
 import { Fotosandvideos } from './components/fotosandvideos'
-import videoFinal from './assets/videoFinal.mp4'
+
 
 
 import './global.css'
+import { VideoFinal } from './videofinal'
 export function App() {
 
   return (
     <div>
       <Introduction />
       <Fotosandvideos />
-      <video controls>
-        <source src={videoFinal}  type="video/mp4" />
-        Seu navegador não suporta o elemento de vídeo.
-      </video>
+      <VideoFinal />
     </div>
   )
 }
